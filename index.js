d4db2d11ced5f9fb3d1fa7c00ca341988b1f14fb
Syed-Umair <syed.yousuff@full.co>
2017-12-04 14:47:40 +0530

    Modified for Our Needs

--- index.js
+++ index.js
@@ -10,20 +10,11 @@ const defaults = {
 	version: 'v4'
 };
 
-function appleCheck(options) {
-	return got('http://captive.apple.com/hotspot-detect.html', {
-		family: options.version === 'v4' ? 4 : 6,
-		headers: {'User-Agent': 'CaptiveNetworkSupport/1.0 wispr'}
-	}).then(res => /Success/.test(res.body || '') || Promise.reject());
-}
-
 module.exports = options => {
 	options = Object.assign({}, defaults, options);
 
 	const p = pAny([
-		publicIp[options.version]().then(() => true),
-		publicIp[options.version]({https: true}).then(() => true),
-		appleCheck(options)
+		publicIp[options.version]({https: true}).then(() => true)
 	]);
 
 	return pTimeout(p, options.timeout).catch(() => false);
--- test-browser.js
+++
@@ -1,8 +0,0 @@
-// Need to test manually in devtools
-// $ browserify test-browser.js | pbcopy
-'use strict';
-const isOnline = require('./browser');
-
-isOnline().then(online => {
-	console.log('online', online);
-});
